2f2513be9bc05681febb8d9f1065039bdf4834c0
Cassio <cassiodegoes@live.com>
2017-11-03 16:22:50 -0200

    lesson 4 final - November, 3th

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 var tempoInicial = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
 
 $(document).ready(function(){
 	atualizaTamanhoFrase();
 	inicializaContadores();
 	inicializaCronometro();
+	$("#botao-reiniciar").click(reiniciaJogo);
 });
+
+/*
+	Essa função funciona com um shortcut para $(document).ready(function)
+
+	$(function(){
+		atualizaTamanhoFrase();
+		inicializaContadores();
+		inicializaCronometro();
+		$("#botao-reiniciar").click(reiniciaJogo);
+	});
+*/
 
 function atualizaTamanhoFrase(){
 	var frase = $(".frase").text();
@@ -12,9 +25,6 @@ function atualizaTamanhoFrase(){
 	var tamanhoFrase = $("#tamanho-frase");
 	tamanhoFrase.text(numPalavras);	
 }
-
-
-var campo = $(".campo-digitacao");
 
 function inicializaContadores(){
 	campo.on("input", function(){
@@ -35,23 +45,25 @@ function inicializaCronometro(){
 	var tempoRestante = $("#tempo-digitacao").text();
 	
 	campo.one("focus", function(){
+		$("#botao-reiniciar").attr("disabled", true);
 		var cronometroID = setInterval(function(){
 			tempoRestante--;
 			$("#tempo-digitacao").text(tempoRestante);
 			if(tempoRestante < 1){
 				campo.attr("disabled", true);
 				clearInterval(cronometroID);
+				$("#botao-reiniciar").attr("disabled", false);
 			}
 		}, 1000);
 	});	
 }
 
-
-$("#botao-reiniciar").click(function(){
+function reiniciaJogo(){
 	campo.val("");
 	campo.attr("disabled", false);
 	$("#contador-palavras").text("0");
 	$("#contador-caracteres").text("0");
 	$("#tempo-digitacao").text(tempoInicial);
 	inicializaCronometro();
-});
+}
+
